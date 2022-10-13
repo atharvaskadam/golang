75992ed0a98e819ac2381103e35f0a89b8407730
atharva kadam <atharva_kadam@bio-rad.com>
2022-10-13 11:41:56 +0530

    Updated Changes

---
+++ readwrite/go.mod
@@ -0,0 +1,3 @@
+module github.com/atharvaskadam/golang/readwrite
+
+go 1.19
---
+++ readwrite/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func writeFile() {
+	file, err := os.Create("Student.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	_, err = file.WriteString("List Of Students")
+	if err != nil {
+		panic(err)
+	}
+}
+func readFile() {
+	data, err := ioutil.ReadFile("Student.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(data)
+}
+func main() {
+	writeFile()
+	readFile()
+}
